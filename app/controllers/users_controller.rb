057e8200643194104aadfa90e4270fe9cadb8ba5
shoaws <108819031+shoaws@users.noreply.github.com>
2022-08-15 14:37:26 +0000

    [Addユーザー更新機能]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
   
   private
