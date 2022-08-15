c7cef015b823daf161715a8fe228278fa2171156
shoaws <108819031+shoaws@users.noreply.github.com>
2022-08-15 03:55:13 +0000

    [Add微調整]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book.id_path
+      redirect_to @book.id_path
     else
       render "user/show"
     end
@@ -22,6 +22,7 @@ class BooksController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = Book.find(params[:id])
+    
   end
 
   def destroy
