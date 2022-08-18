d81c8f5da953b5147eb8f8e3d49cc271d0cbf0ec
shoaws <108819031+shoaws@users.noreply.github.com>
2022-08-18 07:24:14 +0000

    [Addフラッシュメッセージ]

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,6 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
   
+  validates :name, presence: true, length: { minimum: 2, maximum: 20}
+  
 end
